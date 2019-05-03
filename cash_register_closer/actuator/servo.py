cdf5d1a6e3b4e09b647dcf91a165fc5dc6ae9bb3
Felipe Acevedo Montoya <felipe.a.montoya@gmail.com>
2019-05-03 01:48:37 -0500

    Rename attributes for servo class

--- cash_register_closer/actuator/servo.py
+++ cash_register_closer/actuator/servo.py
@@ -6,20 +6,20 @@ import RPi.GPIO as GPIO
 class Servo:
 
     def __init__(self, control_pin, frecuency, initial_position, middle_position):
-        self.control_pin = control_pin
-        self.frecuency = frecuency
-        self.initial_position = initial_position
-        self.middle_position = middle_position
+        self.__control_pin = control_pin
+        self.__frecuency = frecuency
+        self.__initial_position = initial_position
+        self.__middle_position = middle_position
 
         try:
             GPIO.setmode(GPIO.BOARD)
-            GPIO.setup(self.control_pin, GPIO.OUT)
+            GPIO.setup(self.__control_pin, GPIO.OUT)
         except Exception:
             logging.exception("Failed to setup GPIO")
             raise
 
-        self.actuator = GPIO.PWM(self.control_pin, self.frecuency)
-        self.actuator.start(self.initial_position)
+        self.actuator = GPIO.PWM(self.__control_pin, self.__frecuency)
+        self.actuator.start(self.__initial_position)
 
     def move(self, position):
         logging.debug("Moving servo to position '%s'", position)
@@ -27,11 +27,11 @@ class Servo:
 
     def close_register(self):
         time.sleep(0.2)
-        self.move(self.initial_position)
+        self.move(self.__initial_position)
         time.sleep(0.2)
-        self.move(self.middle_position)
+        self.move(self.__middle_position)
         time.sleep(0.2)
-        self.move(self.initial_position)
+        self.move(self.__initial_position)
         self.teardown()
 
     def teardown(self):
