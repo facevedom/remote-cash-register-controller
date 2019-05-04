065ed439a43b4775b633792e5530896b7abf077f
JuanGMejia <JuanGMejia@users.noreply.github.com>
2019-05-03 23:30:59 -0500

    feature/chatBot: chat bot (#2)
    * feature/chatBot: chat bot
    
    * feature/chatBot: refactor code and use chat bot

--- cash_register_closer/app/slack_python.py
+++ cash_register_closer/app/slack_python.py
@@ -1,29 +1,85 @@
+import sqlite3
 import requests
 from flask import Flask
 from flask import request
+from flask import Response
 import os
-import motor_controller
-
+import json
 import threading
 
-# app-server
-app = Flask(__name__)
-# url-app-slack
+conn = sqlite3.connect('./app/cash_register.db')
+c = conn.cursor()
+# c.execute('''CREATE TABLE reservation
+#              (user text, date text, initialHour int, initialMinute int, finishHour int, finishMinute int)''')
+
+# # # Insert a row of data
+# c.execute("INSERT INTO reservation VALUES ('jgmejias' ,'2006-01-05',2,4)")
+# t = (2,2)
+# c.execute('SELECT * FROM reservation WHERE initialHour < ? and finishHour > ?', t)
+# reservation = c.fetchone()
+# print(reservation)
+# if reservation:
+#     print(reservation[0])
+#     print(reservation[2])
+#     print(reservation[3])
+# # Save (commit) the changes
+# conn.commit()
+
+# # We can also close the connection if we are done with it.
+# # Just be sure any changes have been committed or they will be lost.
+# conn.close()
+
+
 URL = os.getenv('SLACK_URL')
+app = Flask(__name__)
 
 # endpoint for process request from slack
-@app.route('/slack', methods=['POST'])
-def login():
+
+def work():
+    return 'work', 200
+
+def close_register():
     text = request.form.get('text')
-    username = request.form.get('user_name')
-    payload = '{"text":"' + username + ' is closing the cash register"}'
-    head = {"Content-type": "application/json"}
+    print(request.form)
+    username = request.form.get('user_id')
+    payload = '{"text":" <@'+ username + '> is closing the cash register"}'
+
+    head = {"Content-type": "application/json"} 
     r = requests.post(url=URL, data=payload, headers=head)
+    # t = threading.Thread(target=motor_controller.rotate)
+    # t.start()
 
-    t = threading.Thread(target=motor_controller.rotate)
-    t.start()
+    return 'we are processing your request', 200
 
-    return username + ' We are processing your request, please wait...', 200
+def processRequest(text):
+    print(text.split(' '))
+    # c.execute('SELECT * FROM reservation WHERE initialHour < ? and finishHour > ?', t)
+    payload = '{"text": "Hello <@'+request.get_json().get('event').get('user')+'> Knock, knock.","channel": "'+request.get_json().get('event').get('channel')+'"}'
+    head = {"Content-type": "application/json", "Authorization": "Bearer xoxb-585478244963-618914840321-GjKFSN1vAF3oqxIApPsTF7dq"} 
+    r = requests.post(url='https://slack.com/api/chat.postMessage', data=payload, headers=head)
 
+def chat_bot():
+    print(request.get_json())
+    if request.get_json().get('challenge'):
+        return request.get_json().get('challenge'), 200
+    if request.get_json().get('event').get('subtype') != 'bot_message' and request.get_json().get('event').get('subtype') != 'message_deleted':
+        text = str(request.get_json().get('event').get('text'))
+        t = threading.Thread(target=processRequest(text))
+        t.start()
+    return 'ok', 200
 
-app.run(debug=True)
+def date():
+    print(json.loads(request.form.get('payload'))['actions'][0]['selected_date'])
+    # text = request.form.get('challenge')
+    return 'recibido', 200
+
+def addRoutes():
+    app.add_url_rule('/slack', 'slack', close_register, methods=['POST'])
+    app.add_url_rule('/chat', 'chat', chat_bot, methods=['POST'])
+    app.add_url_rule('/date', 'date', date, methods=['POST'])
+    app.add_url_rule('/work', 'work', work, methods=['GET'])
+
+def initializeApp():
+    addRoutes()
+initializeApp()
+app.run(debug=True, port=5000)
--- cash_register_closer/main.py
+++ cash_register_closer/main.py
@@ -1,8 +1,6 @@
-from actuator.actuator_manager import ActuatorManager
+# from actuator.actuator_manager import ActuatorManager
 from helpers.logger import logging
 from helpers.config_loader import load_config, config_file_path
-
+from app import slack_python
 config = load_config()
 logging.debug("Using configuration from '%s'", config_file_path)
-
-actuator = ActuatorManager(config)
